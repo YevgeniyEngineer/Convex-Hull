a8e835c5b7c1d5d6a345c8c609a9991eedda068f
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-04-25 20:39:10 +0800

    Minor refactoring

--- test_convex_hull.cpp
+++ test_convex_hull.cpp
@@ -7,6 +7,7 @@
 int main()
 {
     using namespace geom;
+    using PointType = double;
 
     int num_pts = 99'999;
     int coords_range = 1000;
@@ -16,15 +17,58 @@ int main()
     // Seed the random number generator with the current time
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937 generator(seed);
-    std::uniform_real_distribution<double> distribution(0, coords_range);
+    std::uniform_real_distribution<PointType> distribution(0, coords_range);
 
     // Generate random points
-    std::vector<Point<double>> points;
+    std::vector<Point<PointType>> points;
     points.reserve(num_pts);
     for (int i = 0; i < num_pts; ++i)
     {
-        points.push_back(Point<double>(distribution(generator), distribution(generator)));
+        points.push_back(Point<PointType>(distribution(generator), distribution(generator)));
     }
+
+    auto printConvexHullIndices = [](const std::vector<int> &indices, bool print) {
+        if (print)
+        {
+            std::cout << "Convex Hull Indices:" << std::endl;
+            for (int index : indices)
+            {
+                std::cout << index << " ";
+            }
+            std::cout << std::endl;
+        }
+    };
+
+    auto printHullOrientation = [](const std::vector<Point<PointType>> &points, const std::vector<int> &indices) {
+        if (indices.size() < 3)
+        {
+            return;
+        }
+        // Check if points are counterclockwise
+        auto p1 = points[indices[0]];
+        auto p2 = points[indices[1]];
+        auto p3 = points[indices[2]];
+
+        auto orientation = getOrientation(p1, p2, p3);
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
+        {
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
+        }
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
+        {
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
+        }
+    };
 
     // Construct convex hull using GRAHAM SCAN
     {
@@ -32,173 +76,79 @@ int main()
 
         auto t1 = std::chrono::high_resolution_clock::now();
 
-        auto hull_indices1 = constructConvexHull(points, ConvexHullAlgorithm::GRAHAM_SCAN, orientation);
+        auto indices = constructConvexHull(points, ConvexHullAlgorithm::GRAHAM_SCAN, orientation);
 
         auto t2 = std::chrono::high_resolution_clock::now();
 
         std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
-        std::cout << "Number of hull points: " << hull_indices1.size() << std::endl;
+        std::cout << "Number of hull points: " << indices.size() << std::endl;
 
-        // Check if points are counterclockwise
-        auto p1 = points[hull_indices1[0]];
-        auto p2 = points[hull_indices1[1]];
-        auto p3 = points[hull_indices1[2]];
-
-        auto orientation = getOrientation(p1, p2, p3);
-
-        if (orientation == Orientation::COUNTERCLOCKWISE)
-        {
-            std::cout << "Hull is oriented counterclockwise" << std::endl;
-        }
-        else if (orientation == Orientation::CLOCKWISE)
-        {
-            std::cout << "Hull is oriented clockwise" << std::endl;
-        }
+        // Print hull orientation
+        printHullOrientation(points, indices);
 
         // Print hull indices
-        if (print_results)
-        {
-            std::cout << "Convex Hull Indices:" << std::endl;
-            for (int index : hull_indices1)
-            {
-                std::cout << index << " ";
-            }
-            std::cout << std::endl;
-        }
+        printConvexHullIndices(indices, print_results);
     }
 
     // Construct convex hull using ANDREW MONOTONE CHAIN
     {
         std::cout << std::endl << "Constructing convex hull using ANDREW MONOTONE CHAIN " << std::endl;
 
-        auto t3 = std::chrono::high_resolution_clock::now();
+        auto t1 = std::chrono::high_resolution_clock::now();
 
-        auto hull_indices2 = constructConvexHull(points, ConvexHullAlgorithm::ANDREW_MONOTONE_CHAIN, orientation);
+        auto indices = constructConvexHull(points, ConvexHullAlgorithm::ANDREW_MONOTONE_CHAIN, orientation);
 
-        auto t4 = std::chrono::high_resolution_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
 
-        std::cout << "Elapsed time (s): " << (t4 - t3).count() / 1e9 << std::endl;
-        std::cout << "Number of hull points: " << hull_indices2.size() << std::endl;
+        std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << indices.size() << std::endl;
 
-        // Check if points are counterclockwise
-        auto p1 = points[hull_indices2[0]];
-        auto p2 = points[hull_indices2[1]];
-        auto p3 = points[hull_indices2[2]];
-
-        auto orientation = getOrientation(p1, p2, p3);
-
-        if (orientation == Orientation::COUNTERCLOCKWISE)
-        {
-            std::cout << "Hull is oriented counterclockwise" << std::endl;
-        }
-        else if (orientation == Orientation::CLOCKWISE)
-        {
-            std::cout << "Hull is oriented clockwise" << std::endl;
-        }
+        // Print hull orientation
+        printHullOrientation(points, indices);
 
         // Print hull indices
-        if (print_results)
-        {
-            std::cout << "Convex Hull Indices:" << std::endl;
-            for (int index : hull_indices2)
-            {
-                std::cout << index << " ";
-            }
-            std::cout << std::endl;
-        }
+        printConvexHullIndices(indices, print_results);
     }
 
     // Construct convex hull using JARVIS MARCH
     {
         std::cout << std::endl << "Constructing convex hull using JARVIS MARCH " << std::endl;
 
-        auto t5 = std::chrono::high_resolution_clock::now();
+        auto t1 = std::chrono::high_resolution_clock::now();
 
-        auto hull_indices3 = constructConvexHull(points, ConvexHullAlgorithm::JARVIS_MARCH, orientation);
+        auto indices = constructConvexHull(points, ConvexHullAlgorithm::JARVIS_MARCH, orientation);
 
-        auto t6 = std::chrono::high_resolution_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
 
-        std::cout << "Elapsed time (s): " << (t6 - t5).count() / 1e9 << std::endl;
-        std::cout << "Number of hull points: " << hull_indices3.size() << std::endl;
+        std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << indices.size() << std::endl;
 
-        // Check if points are counterclockwise
-        auto p1 = points[hull_indices3[0]];
-        auto p2 = points[hull_indices3[1]];
-        auto p3 = points[hull_indices3[2]];
-
-        auto orientation = getOrientation(p1, p2, p3);
-
-        if (orientation == Orientation::COUNTERCLOCKWISE)
-        {
-            std::cout << "Hull is oriented counterclockwise" << std::endl;
-        }
-        else if (orientation == Orientation::CLOCKWISE)
-        {
-            std::cout << "Hull is oriented clockwise" << std::endl;
-        }
+        // Print hull orientation
+        printHullOrientation(points, indices);
 
         // Print hull indices
-        if (print_results)
-        {
-            std::cout << "Convex Hull Indices:" << std::endl;
-            for (int index : hull_indices3)
-            {
-                std::cout << index << " ";
-            }
-            std::cout << std::endl;
-        }
+        printConvexHullIndices(indices, print_results);
     }
 
     // Construct convex hull using CHAN's algorithm
     {
         std::cout << std::endl << "Constructing convex hull using CHAN " << std::endl;
 
-        auto t7 = std::chrono::high_resolution_clock::now();
+        auto t1 = std::chrono::high_resolution_clock::now();
 
-        auto hull_indices4 = constructConvexHull(points, ConvexHullAlgorithm::CHAN, orientation);
+        auto indices = constructConvexHull(points, ConvexHullAlgorithm::CHAN, orientation);
 
-        auto t8 = std::chrono::high_resolution_clock::now();
+        auto t2 = std::chrono::high_resolution_clock::now();
 
-        std::cout << "Elapsed time (s): " << (t8 - t7).count() / 1e9 << std::endl;
-        std::cout << "Number of hull points: " << hull_indices4.size() << std::endl;
+        std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << indices.size() << std::endl;
 
-        // Check if points are counterclockwise
-        auto p1 = points[hull_indices4[0]];
-        auto p2 = points[hull_indices4[1]];
-        auto p3 = points[hull_indices4[2]];
-
-        auto orientation = getOrientation(p1, p2, p3);
-
-        if (orientation == Orientation::COUNTERCLOCKWISE)
-        {
-            std::cout << "Hull is oriented counterclockwise" << std::endl;
-        }
-        else if (orientation == Orientation::CLOCKWISE)
-        {
-            std::cout << "Hull is oriented clockwise" << std::endl;
-        }
+        // Print hull orientation
+        printHullOrientation(points, indices);
 
         // Print hull indices
-        if (print_results)
-        {
-            std::cout << "Convex Hull Indices:" << std::endl;
-            for (int index : hull_indices4)
-            {
-                std::cout << index << " ";
-            }
-            std::cout << std::endl;
-        }
+        printConvexHullIndices(indices, print_results);
     }
-
-    // // Print hull indices
-    // if (print_results)
-    // {
-    //     std::cout << "Convex Hull Indices:" << std::endl;
-    //     for (int index : hull_indices3)
-    //     {
-    //         std::cout << index << std::endl;
-    //     }
-    // }
 
     return 0;
 }
