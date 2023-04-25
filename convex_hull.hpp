480a716144bad8ecf4720c0e1eb9370b2eb41d8a
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-04-25 20:04:03 +0800

    Need to figure out why Chan's algorithm produces less points

--- convex_hull.hpp
+++ convex_hull.hpp
@@ -3,12 +3,33 @@
 
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 #include <limits>
+#include <numeric>
 #include <type_traits>
+#include <variant>
 #include <vector>
 
 namespace geom
 {
+/// @brief Class that stores orientation formed by three 2D points
+enum class Orientation
+{
+    COUNTERCLOCKWISE,
+    CLOCKWISE,
+    COLINEAR
+};
+
+/// @brief Class that stores available Convex Hull algorithms for selection
+enum class ConvexHullAlgorithm
+{
+    GRAHAM_SCAN,
+    ANDREW_MONOTONE_CHAIN,
+    JARVIS_MARCH,
+    CHAN
+};
+
+/// @brief Point type used in calculation of 2D Convex Hull
 template <typename T> struct Point
 {
     static_assert(std::is_integral<T>::value || std::is_floating_point<T>::value,
@@ -16,7 +37,7 @@ template <typename T> struct Point
 
     T x, y;
 
-    Point(T x, T y) : x(x), y(y)
+    constexpr Point(T x, T y) : x(x), y(y)
     {
     }
 
@@ -76,14 +97,6 @@ inline bool comparePolarAngle(const Point<T> &ref, const Point<T> &p1, const Poi
     return (cross_product > 0);
 }
 
-/// @brief Class that stores orientation formed by three 2D points
-enum class Orientation
-{
-    COUNTERCLOCKWISE,
-    CLOCKWISE,
-    COLINEAR
-};
-
 /// @brief Function to check if three points form a clockwise or counterclockwise order
 template <typename T> Orientation getOrientation(const Point<T> &p1, const Point<T> &p2, const Point<T> &p3) noexcept
 {
@@ -103,17 +116,9 @@ template <typename T> Orientation getOrientation(const Point<T> &p1, const Point
     }
 }
 
-/// @brief Class that stores available Convex Hull algorithms for selection
-enum class ConvexHullAlgorithm
-{
-    GRAHAM_ANDREW,
-    JARVIS_MARCH,
-    CHAN
-};
-
 /// @brief Return convex hull indices calculated using Graham-Andrew algorithm
 template <typename T>
-std::vector<int> constructGrahamAndrewConvexHull(const std::vector<Point<T>> &points, Orientation orientation)
+std::vector<int> constructGrahamScanConvexHull(const std::vector<Point<T>> &points, Orientation orientation)
 {
     int n = static_cast<int>(points.size());
     if (n < 3)
@@ -170,8 +175,7 @@ std::vector<int> constructGrahamAndrewConvexHull(const std::vector<Point<T>> &po
         const auto hull_index = hull_indices[i];
         for (int j = 0; j < n; ++j)
         {
-            const auto &point = points[j];
-            if (sorted_points[hull_index] == point)
+            if (sorted_points[hull_index] == points[j])
             {
                 original_indices[i] = j;
                 break;
@@ -179,29 +183,238 @@ std::vector<int> constructGrahamAndrewConvexHull(const std::vector<Point<T>> &po
         }
     }
 
+    // Check orientation, and reverse order if orientation set to CLOCKWISE
     if (orientation == Orientation::CLOCKWISE)
     {
         std::reverse(original_indices.begin(), original_indices.end());
     }
-
     return original_indices;
 }
 
+/// @brief Andrew's Monotone Chain convex hull algorithm
+template <typename T>
+std::vector<int> constructAndrewMonotoneChainConvexHull(const std::vector<Point<T>> &points, Orientation orientation)
+{
+    int n = static_cast<int>(points.size());
+    if (n < 3)
+    {
+        // Convex Hull is not possible for less than 3 points
+        return {};
+    }
+
+    // Preallocate indices and sort points
+    std::vector<int> hull_indices(2 * n);
+    std::vector<Point<T>> sorted_points = points;
+    std::sort(sorted_points.begin(), sorted_points.end());
+
+    // Compute lower hull
+    int k = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        while (k >= 2 && getOrientation(sorted_points[hull_indices[k - 2]], sorted_points[hull_indices[k - 1]],
+                                        sorted_points[i]) != Orientation::COUNTERCLOCKWISE)
+        {
+            --k;
+        }
+        hull_indices[k++] = i;
+    }
+
+    // Compute upper hull
+    for (int i = n - 2, t = k + 1; i >= 0; --i)
+    {
+        while (k >= t && getOrientation(sorted_points[hull_indices[k - 2]], sorted_points[hull_indices[k - 1]],
+                                        sorted_points[i]) != Orientation::COUNTERCLOCKWISE)
+        {
+            --k;
+        }
+        hull_indices[k++] = i;
+    }
+
+    // Resize convex hull to contain only required points
+    hull_indices.resize(k - 1);
+
+    // Convert the indices of the sorted_points vector back to the original points vector
+    for (int i = 0; i < k - 1; ++i)
+    {
+        const auto hull_index = hull_indices[i];
+        for (int j = 0; j < n; ++j)
+        {
+            if (sorted_points[hull_index] == points[j])
+            {
+                hull_indices[i] = j;
+                break;
+            }
+        }
+    }
+
+    // Check orientation, and reverse order if orientation set to CLOCKWISE
+    if (orientation == Orientation::CLOCKWISE)
+    {
+        std::reverse(hull_indices.begin(), hull_indices.end());
+    }
+    return hull_indices;
+}
+
+/// @brief Construct Convex Hull using Jarvis March algorithm
+template <typename T>
+std::vector<int> constructJarvisMarchConvexHull(const std::vector<Point<T>> &points, Orientation orientation)
+{
+    int n = static_cast<int>(points.size());
+    if (n < 3)
+    {
+        // Convex Hull is not possible for less than 3 points
+        return {};
+    }
+
+    // Find the leftmost point (with the lowest x-coordinate)
+    int leftmost = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        if (points[i].x < points[leftmost].x)
+        {
+            leftmost = i;
+        }
+    }
+
+    // Start from leftmost point, keep moving counterclockwise
+    // until reaching the start point again.
+    std::vector<int> hull_indices;
+    int p = leftmost;
+    int q;
+
+    do
+    {
+        // Add current point to result
+        hull_indices.push_back(p);
+
+        // Initialize q as the next point in the list
+        q = (p + 1) % n;
+
+        // Iterate over all points to find the point with the smallest polar angle
+        // with respect to the current point (p)
+        for (int i = 0; i < n; ++i)
+        {
+            // If i is more counterclockwise than current q, then update q
+            if (getOrientation(points[p], points[i], points[q]) == Orientation::COUNTERCLOCKWISE)
+            {
+                q = i;
+            }
+        }
+
+        // Set p to be q for the next iteration
+        p = q;
+
+    } while (p != leftmost); // Continue until we reach the starting point again
+
+    // Check orientation, and reverse order if orientation set to CLOCKWISE
+    if (orientation == Orientation::CLOCKWISE)
+    {
+        std::reverse(hull_indices.begin(), hull_indices.end());
+    }
+    return hull_indices;
+}
+
+/// @brief Partitions vector into approximately n equal pieces
+template <typename T> std::vector<std::vector<T>> partitionVector(const std::vector<T> &vec, int number_of_subsets)
+{
+    int size = vec.size();
+    int subset_size = size / number_of_subsets;
+    int remainder = size % number_of_subsets;
+
+    std::vector<std::vector<T>> subsets(number_of_subsets);
+    int current = 0;
+    for (int i = 0; i < number_of_subsets; ++i)
+    {
+        int current_subset_size = subset_size + (i < remainder ? 1 : 0);
+        subsets[i].reserve(current_subset_size);
+        for (int j = 0; j < current_subset_size; ++j)
+        {
+            subsets[i].push_back(vec[current++]);
+        }
+    }
+
+    return subsets;
+}
+
+/// @brief Construct Convex Hull using Chan's algorithm, based on Andrew's Monotone Chain and Jarvis March
+template <typename T> std::vector<int> constructChanConvexHull(std::vector<Point<T>> points, Orientation orientation)
+{
+    int n = static_cast<int>(points.size());
+    if (n < 3)
+    {
+        // Convex Hull is not possible for less than 3 points
+        return {};
+    }
+
+    // Find the number of subsets to split the points into
+    // const int number_of_subsets = static_cast<int>(std::ceil(std::sqrt(n)));
+    const int number_of_subsets = static_cast<int>(std::ceil(std::log(n) / std::log(2)));
+    const int number_of_points_within_subset = n / number_of_subsets;
+
+    // Divide points into number_of_subsets subsets each containing number_of_points_within_subset points
+    const auto subsets = partitionVector(points, number_of_subsets);
+
+    // Compute convex hull using Graham's scan for each subset
+    std::vector<std::vector<int>> convex_hulls_indices(subsets.size());
+    for (int subset_no = 0; subset_no < subsets.size(); ++subset_no)
+    {
+        // Construct convex hull for current subset
+        convex_hulls_indices[subset_no] = constructAndrewMonotoneChainConvexHull(subsets[subset_no], orientation);
+    }
+
+    // Merge convex hull points
+    std::vector<Point<T>> merged_points;
+    merged_points.reserve(convex_hulls_indices.size() * number_of_points_within_subset);
+
+    std::vector<int> merged_indices;
+    merged_indices.reserve(convex_hulls_indices.size() * number_of_points_within_subset);
+
+    for (const auto &indices : convex_hulls_indices)
+    {
+        for (const int idx : indices)
+        {
+            merged_points.push_back(points[idx]);
+            merged_indices.push_back(idx);
+        }
+    }
+
+    // Merge convex hulls using Jarvis March and obtain the indices of the points in the hull
+    std::vector<int> hull_indices = constructJarvisMarchConvexHull(merged_points, orientation);
+
+    // Convert the indices of the merged_points vector back to the original points vector
+    std::vector<int> original_indices(hull_indices.size());
+    for (size_t i = 0; i < hull_indices.size(); ++i)
+    {
+        original_indices[i] = merged_indices[hull_indices[i]];
+    }
+    return original_indices;
+}
+
+/// @brief Main method that calls relevant functions based on provided inputs
 template <typename T>
 std::vector<int> constructConvexHull(const std::vector<Point<T>> &points,
-                                     ConvexHullAlgorithm algorithm = ConvexHullAlgorithm::GRAHAM_ANDREW,
+                                     ConvexHullAlgorithm algorithm = ConvexHullAlgorithm::GRAHAM_SCAN,
                                      Orientation orientation = Orientation::COUNTERCLOCKWISE)
 {
+    if (orientation == Orientation::COLINEAR)
+    {
+        std::cerr << "Orientation::COLINEAR only supported internally. Returning empty hull." << std::endl;
+        return {};
+    }
+
     switch (algorithm)
     {
-    case ConvexHullAlgorithm::GRAHAM_ANDREW: {
-        return constructGrahamAndrewConvexHull(points, orientation);
+    case ConvexHullAlgorithm::GRAHAM_SCAN: {
+        return constructGrahamScanConvexHull(points, orientation);
+    }
+    case ConvexHullAlgorithm::ANDREW_MONOTONE_CHAIN: {
+        return constructAndrewMonotoneChainConvexHull(points, orientation);
     }
     case ConvexHullAlgorithm::JARVIS_MARCH: {
-        return {};
+        return constructJarvisMarchConvexHull(points, orientation);
     }
     case ConvexHullAlgorithm::CHAN: {
-        return {};
+        return constructChanConvexHull(points, orientation);
     }
     default: {
         return {};
--- test_convex_hull.cpp
+++ test_convex_hull.cpp
@@ -8,9 +8,10 @@ int main()
 {
     using namespace geom;
 
-    int num_pts = 100'000;
+    int num_pts = 99'999;
     int coords_range = 1000;
     bool print_results = false;
+    auto orientation = Orientation::CLOCKWISE;
 
     // Seed the random number generator with the current time
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -25,42 +26,135 @@ int main()
         points.push_back(Point<double>(distribution(generator), distribution(generator)));
     }
 
-    // Construct convex hull
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    std::vector<int> hull_indices =
-        constructConvexHull(points, ConvexHullAlgorithm::GRAHAM_ANDREW, Orientation::CLOCKWISE);
-
-    auto t2 = std::chrono::high_resolution_clock::now();
-
-    std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
-    std::cout << "Number of hull points: " << hull_indices.size() << std::endl;
-
-    // Check if points are counterclockwise
-    auto p1 = points[hull_indices[0]];
-    auto p2 = points[hull_indices[1]];
-    auto p3 = points[hull_indices[2]];
-
-    auto orientation = getOrientation(p1, p2, p3);
-
-    if (orientation == Orientation::COUNTERCLOCKWISE)
+    // Construct convex hull using GRAHAM SCAN
     {
-        std::cout << "Hull is oriented counterclockwise" << std::endl;
-    }
-    else if (orientation == Orientation::CLOCKWISE)
-    {
-        std::cout << "Hull is oriented clockwise" << std::endl;
-    }
+        std::cout << std::endl << "Constructing convex hull using GRAHAM SCAN " << std::endl;
 
-    // Print hull indices
-    if (print_results)
-    {
-        std::cout << "Convex Hull Indices:" << std::endl;
-        for (int index : hull_indices)
+        auto t1 = std::chrono::high_resolution_clock::now();
+
+        auto hull_indices1 = constructConvexHull(points, ConvexHullAlgorithm::GRAHAM_SCAN, orientation);
+
+        auto t2 = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Elapsed time (s): " << (t2 - t1).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << hull_indices1.size() << std::endl;
+
+        // Check if points are counterclockwise
+        auto p1 = points[hull_indices1[0]];
+        auto p2 = points[hull_indices1[1]];
+        auto p3 = points[hull_indices1[2]];
+
+        auto orientation = getOrientation(p1, p2, p3);
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
         {
-            std::cout << index << std::endl;
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
         }
     }
+
+    // Construct convex hull using ANDREW MONOTONE CHAIN
+    {
+        std::cout << std::endl << "Constructing convex hull using ANDREW MONOTONE CHAIN " << std::endl;
+
+        auto t3 = std::chrono::high_resolution_clock::now();
+
+        auto hull_indices2 = constructConvexHull(points, ConvexHullAlgorithm::ANDREW_MONOTONE_CHAIN, orientation);
+
+        auto t4 = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Elapsed time (s): " << (t4 - t3).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << hull_indices2.size() << std::endl;
+
+        // Check if points are counterclockwise
+        auto p1 = points[hull_indices2[0]];
+        auto p2 = points[hull_indices2[1]];
+        auto p3 = points[hull_indices2[2]];
+
+        auto orientation = getOrientation(p1, p2, p3);
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
+        {
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
+        }
+    }
+
+    // Construct convex hull using JARVIS MARCH
+    {
+        std::cout << std::endl << "Constructing convex hull using JARVIS MARCH " << std::endl;
+
+        auto t5 = std::chrono::high_resolution_clock::now();
+
+        auto hull_indices3 = constructConvexHull(points, ConvexHullAlgorithm::JARVIS_MARCH, orientation);
+
+        auto t6 = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Elapsed time (s): " << (t6 - t5).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << hull_indices3.size() << std::endl;
+
+        // Check if points are counterclockwise
+        auto p1 = points[hull_indices3[0]];
+        auto p2 = points[hull_indices3[1]];
+        auto p3 = points[hull_indices3[2]];
+
+        auto orientation = getOrientation(p1, p2, p3);
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
+        {
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
+        }
+    }
+
+    // Construct convex hull using CHAN's algorithm
+    {
+        std::cout << std::endl << "Constructing convex hull using CHAN " << std::endl;
+
+        auto t7 = std::chrono::high_resolution_clock::now();
+
+        auto hull_indices4 = constructConvexHull(points, ConvexHullAlgorithm::CHAN, orientation);
+
+        auto t8 = std::chrono::high_resolution_clock::now();
+
+        std::cout << "Elapsed time (s): " << (t8 - t7).count() / 1e9 << std::endl;
+        std::cout << "Number of hull points: " << hull_indices4.size() << std::endl;
+
+        // Check if points are counterclockwise
+        auto p1 = points[hull_indices4[0]];
+        auto p2 = points[hull_indices4[1]];
+        auto p3 = points[hull_indices4[2]];
+
+        auto orientation = getOrientation(p1, p2, p3);
+
+        if (orientation == Orientation::COUNTERCLOCKWISE)
+        {
+            std::cout << "Hull is oriented counterclockwise" << std::endl;
+        }
+        else if (orientation == Orientation::CLOCKWISE)
+        {
+            std::cout << "Hull is oriented clockwise" << std::endl;
+        }
+    }
+
+    // // Print hull indices
+    // if (print_results)
+    // {
+    //     std::cout << "Convex Hull Indices:" << std::endl;
+    //     for (int index : hull_indices3)
+    //     {
+    //         std::cout << index << std::endl;
+    //     }
+    // }
 
     return 0;
 }
